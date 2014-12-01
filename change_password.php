3010f72663b5554fb99388dd64d7fb789e69b530
ramyaps <rpaartha@umich.edu>
2014-12-01 18:31:27 -0500

    Commiting update_personal_info.php after modification Created change_password.php to change password Modified account_menu.php

---
+++ change_password.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+include_once('includes/connection.php');
+include_once('includes/user.php');
+$user = new User();
+
+if(isset($_SESSION['logged_in'])) {
+    $page_title = "Change Password";
+    include('includes/header.php');
+    $user_id = $_SESSION['user_id'];
+    $profile = $user->fetch_user($user_id);
+    $heading = "";
+    $cpwd = "";
+    $npwd = "" ;
+    $confpwd = "";
+    $action = isset($_GET['action']) ? $_GET['action'] : "";
+    if($action == 'change'){
+        $cpwd = isset($_POST['cpwd']) ? $_POST['cpwd'] : "" ;
+        $npwd = isset($_POST['npwd']) ? $_POST['npwd'] : "" ;
+        $confpwd = isset($_POST['confpwd']) ? $_POST['confpwd'] : "" ;
+
+        $hcpwd = md5($cpwd);
+        $hnpwd = md5($npwd);
+        $hconfpwd = md5($confpwd);
+
+        if($hcpwd != $profile['password']){
+            $error = "Incorrect current password";
+        }elseif($hnpwd != $hconfpwd){
+            $error = "New password and confirm password do not match";
+        }else {
+            $query = $pdo->prepare("UPDATE user SET password = ? WHERE id = ?");
+            $query->bindValue(1, $hnpwd);
+            $query->bindValue(2, $user_id);
+            $query->execute() or die(print_r($query->errorInfo()));
+
+            $heading = "Password changed successfully";
+            $cpwd = $npwd = $confpwd = "";
+        }
+    }
+?>
+    <br>&nbsp;&nbsp;<a href="account_menu.php" id="account">Your Account</a>&nbsp;&gt;&nbsp;<span style='color: indianred'>Change Password</span>
+    <div class='box'>
+        <div class="left_column"></div>
+        <div class='center_column'>
+            <h3><?php echo $heading ?></h3>
+            <?php if (isset($error)) {?>
+                <p class="error_info"><?php echo $error;?></p>
+            <?php } ?>
+        <form method="post" action="change_password.php?action=change">
+            <p><label class="input_label">Current Password</label><input type="password" name="cpwd" value="<?php echo $cpwd?>" required></p>
+            <p><label class="input_label">New Password</label><input type="password" name="npwd" value="<?php echo $npwd?>" required></p>
+            <p><label class="input_label">Confirm Password</label><input type="password" name="confpwd" value="<?php echo $confpwd ?>" required></p>
+            <input type="submit" value="Change Password">&nbsp;<input type="reset" value="Cancel">&nbsp;
+            <a href="account_menu.php"><input type="button" value="Back"></a>
+        </form>
+        </div>
+
+<?php
+    include('includes/footer.php');
+}else {
+    include('signin.php');
+}
+?>
--- update_personal_info.php
+++ update_personal_info.php
@@ -1,12 +1,113 @@
 <?php
 session_start();
+include_once('includes/connection.php');
+include_once('includes/user.php');
+$user = new User();
 
 if(isset($_SESSION['logged_in'])) {
+    $page_title = "Update Personal Info";
     include('includes/header.php');
+    $id = $_SESSION['user_id'];     //fetch user id
+    $heading = "";
+    $action = isset($_GET['action']) ? $_GET['action'] : "";
+    if ($action == 'updateinfo'){
+        $nname = (!empty($_POST['nname']) ? $_POST['nname'] : null);
+        $firstName = (!empty($_POST['firstName']) ? $_POST['firstName'] : null);
+        $lastName = (!empty($_POST['lastName']) ? $_POST['lastName'] : null);
+        $id = $_POST['user_id'];
+        $phone = (!empty($_POST['phone']) ? $_POST['phone'] : null);
+        $address = (!empty($_POST['address']) ? $_POST['address'] : null);
+
+        $query = $pdo->prepare("UPDATE user SET nick_name = :nname, phone = :phone, first_name = :first_name, last_name = :last_name, address = :address WHERE id = :id");
+        $query->bindParam(':nname', $nname, PDO::PARAM_STR);
+        $query->bindParam(':phone', $phone, PDO::PARAM_STR);
+        $query->bindParam(':first_name', $firstName, PDO::PARAM_STR);
+        $query->bindParam(':last_name', $lastName, PDO::PARAM_STR);
+        $query->bindParam(':address', $address, PDO::PARAM_STR);
+        $query->bindParam(':id', $id, PDO::PARAM_INT);
+        $query->execute() or die(print_r($query->errorInfo()));
+        $heading = "Updated Successfully";
+    }
+
+//    $sql = "SELECT * FROM user where id = ?";
+//    $r = $pdo->query($sql);
+    $row = $user->fetch_user($id);
 ?>
-       <br><br><br>
-        <p>update personal info </p>
-	
+    <br>&nbsp;&nbsp;<a href="account_menu.php" id="account">Your Account</a>&nbsp;&gt;&nbsp;<span style='color: indianred'>Update Personal Info</span>
+    <div class='box'>
+        <div class="left_column"></div>
+        <div class='center_column'>
+            <h3><?php echo $heading ?></h3>
+            <em class="comments">Fields marked with * are mandatory</em>
+            <form  method="POST" action="update_personal_info.php?action=updateinfo">
+                <input type="hidden" name='user_id' value='<?php echo $id ?>'>
+                <table>
+                    <tbody>
+                        <p>
+                            <label class='input_label'>User:</label>
+                            <input type="text" id="email" name="email" value="<?php echo htmlspecialchars($row['email']); ?>" readonly>
+                        </p>
+                        <p><label class='input_label'>Nick Name:</label>
+                            <input type="text" id="nname" name="nname" value="<?php echo htmlspecialchars($row['nick_name']); ?>" pattern="^[A-Za-z ]+$" required>
+                            <em class="comments">*</em>
+                        </p>
+
+                        <p><label class='input_label'>First name: </label>
+                            <input type="text" id="firstName" name="firstName"
+                                   value="<?php echo htmlspecialchars($row['first_name']); ?>" pattern="^[A-Za-z]{1,20}$" required>
+                            <em class="comments">*Alpha only;Limit to 20char</em>
+                        </p>
+                        <p><label class='input_label'>Last Name:</label>
+                            <input type="text" id="lastName" name="lastName"
+                                   value="<?php echo htmlspecialchars($row['last_name']); ?>" pattern="^[A-Za-z]{1,20}$" required>
+                            <em class="comments">*Alpha only;Limit to 20char</em>
+                        </p>
+                        <p><label class='input_label'>Phone:</label>
+                            <input type="text" id="phone" name="phone" value="<?php echo htmlspecialchars($row['phone']); ?>" pattern="^[0-9]{6,12}$" required>
+                            <em class="comments">*Numbers only</em>
+                        </p>
+                        <p><label class='input_label'>Address:</label>
+                            <input type="text" id="address" name="address"
+                                                     value="<?php echo htmlspecialchars($row['address']); ?>" required>
+                            <em class="comments">*</em>
+                        </p>
+
+                        <input type="submit" name="Update Info" value="Update Info" > <!--onclick="setbutton();"-->
+                        &nbsp;<a href="account_menu.php"><input type="button" name="back" value="Back"></a>
+                    </tbody>
+                </table>
+            </form>
+
+        </div>
+    </div>
+
+    <!-- Script to validate data -->
+    <script  type="text/javascript">
+        function setbutton(){
+
+            var firstNamejs = document.forms[0].firstName;
+            var lastNamejs = document.forms[0].lastName;
+            var emailjs = document.forms[0].email;
+            var phonejs = document.forms[0].phone;
+            var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+            var emailText = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            var letters = /^[A-Za-z]+$/;
+
+            if(firstNamejs.value.match(letters)&&
+                lastNamejs.value.match(letters)
+                && (emailText.test(emailjs.value))
+                && phonejs.value.match(phoneno))
+            {
+                document.forms[0].submit();
+            }
+            else
+            {
+                alert('Please correct inputs!');
+                return false;
+            }
+        }
+
+    </script>
 <?php
     include('includes/footer.php');
 } else {
