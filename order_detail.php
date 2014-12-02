742acc6a7ca8a3c6d2c2fda1b2c97fb4782e2057
ramyaps <rpaartha@umich.edu>
2014-12-01 23:27:56 -0500

    Modified and Completed sold_history.php changed order_details.php to display for seller and buyer profile.

--- order_detail.php
+++ order_detail.php
@@ -10,6 +10,7 @@ $user = new User();
 
 //Check if the user has logged in
 if(isset($_SESSION['logged_in'])) {
+    $user_id = $_SESSION['user_id'];
     if(isset($_GET['order_id'])) {
         $order_id = $_GET['order_id'];
 
@@ -37,7 +38,17 @@ if(isset($_SESSION['logged_in'])) {
 
             $review = $query->fetch(PDO::FETCH_ASSOC);
         }
+
+        //set filter to order status - to be used in navigating back to order history page
         $filter = $order_data['status'];
+
+        //set the user profile to buyer or seller - to be used in modifying data displayed in order details
+        //also for navigating back to order history or sold history based on user profile.
+        if($user_id == $order_data['buyer_id']){
+            $profile = 'buyer';
+        } elseif($user_id == $product_data['user_id']){
+            $profile = 'seller';
+        }
 
         $page_title = "Webshelf- Order Detail";
         include_once('includes/header.php');
@@ -46,7 +57,14 @@ if(isset($_SESSION['logged_in'])) {
 
     <br xmlns="http://www.w3.org/1999/html">
     &nbsp;&nbsp;<a href="account_menu.php" id="account">Your Account</a>&nbsp;&gt;&nbsp;
-    <a href="order_history.php">Orders</a>&nbsp;&gt;&nbsp;<span style="color: indianred">Order Details </span>
+    <?php
+        if($profile == 'buyer') {
+            print("<a href='order_history.php'>Orders</a>&nbsp;&gt;&nbsp;");
+        }elseif($profile == 'seller'){
+            print("<a href='sold_history.php'>Sold History</a>&nbsp;&gt;&nbsp;");
+        }
+    ?>
+    <span style="color: indianred">Order Details </span>
     <br>
     <ul class="nostyle_horizontal box">
         <li>
@@ -74,20 +92,51 @@ if(isset($_SESSION['logged_in'])) {
         <div class="left_column display_inline">
             <p><span style="color: coral;font-weight: bold">Order Status:</span> <?php echo $order_data['status']?></p>
             <img height="auto" width="100px" src="<?php echo $image?>" alt="product image" class="product">
-            <p><a href="order_history.php?filter=<?php echo $filter?>"><input type='button' value='Back'></a></p>
+            <p>
+                <?php if($profile == 'buyer'){ ?>
+                    <a href="order_history.php?filter=<?php echo $filter?>">
+                <?php }elseif($profile == 'seller'){ ?>
+                    <a href="sold_history.php">
+                <?php } ?>
+                    <input type='button' value='Back'>
+                </a>
+            </p>
         </div>
-        <div class="display_inline center_column text_wrap">
-            <h3><?php echo $product_data['name']?></h3>
-            <p>Seller: &nbsp;<?php echo $seller_data['first_name']." ".$seller_data['last_name'] ?></p>
 
-            <p>Email: <?php echo $seller_data['email']?></p>
+    <?php
+        if($profile == 'buyer') {
+    ?>
+        <div class="display_inline center_column text_wrap">
+            <h3><?php echo $product_data['name'] ?></h3>
+
+            <p>Seller: &nbsp;<?php echo $seller_data['first_name'] . " " . $seller_data['last_name'] ?></p>
+
+            <p>Email: <?php echo $seller_data['email'] ?></p>
+
             <form method="post" action="message.php">
-                <input type="hidden" name="receiver_id" value="<?php echo $seller_data['id']?>">
+                <input type="hidden" name="receiver_id" value="<?php echo $seller_data['id'] ?>">
                 Contact Seller <input type="submit" name="msg_submit" value="Send Message">
             </form>
         </div>
+    <?php
+        }elseif($profile =='seller') {
+    ?>
+        <div class="display_inline center_column text_wrap">
+            <h3><?php echo $product_data['name'] ?></h3>
 
-<?php
+            <p>Buyer: &nbsp;<?php echo $buyer_data['first_name'] . " " . $buyer_data['last_name'] ?></p>
+
+            <p>Email: <?php echo $buyer_data['email'] ?></p>
+
+            <form method="post" action="message.php">
+                <input type="hidden" name="receiver_id" value="<?php echo $buyer_data['id'] ?>">
+                Contact Buyer <input type="submit" name="msg_submit" value="Send Message">
+            </form>
+        </div>
+    <?php
+        }
+     ?>
+    <?php
         print("<div class='right_column display_inline text_wrap'>");
             print("<p><span style='color: coral;font-weight: bold'>Review:</span></p>");
             if(!empty($review)){
@@ -100,15 +149,16 @@ if(isset($_SESSION['logged_in'])) {
             /*Users can cancel those orders that are not completed yet. And they can provide feedback for only those orders
             that are delivered and completed.
             If the order status is still "ORDERED", provide user a link to cancel it.*/
+            if($profile == 'buyer'){
+                if($order_data['status'] == 'Ordered'){
+                    print("<input type='button' onclick='toggleCancel(1);' value='Cancel Order'>");
+                }
 
-            if($order_data['status'] == 'Ordered'){
-                print("<input type='button' onclick='toggleCancel(1);' value='Cancel Order'>");
-            }
-
-            //if the order is already delivered & completed, provide user link to give feedback if not already given
-            elseif($order_data['status'] == 'Delivered') {
-                if(empty($review)) {
-                    print("<a href='feedback.php?order_id=$order_id'>Leave feedback</a><br>");
+                //if the order is already delivered & completed, provide user link to give feedback if not already given
+                elseif($order_data['status'] == 'Delivered') {
+                    if(empty($review)) {
+                        print("<a href='feedback.php?order_id=$order_id'>Leave feedback</a><br>");
+                    }
                 }
             }
         print("</div>");
@@ -119,7 +169,7 @@ if(isset($_SESSION['logged_in'])) {
             print("<a href='order.php?action=cancel&order_id=".$order_data['id']."'>Yes</a>&nbsp;&nbsp;&nbsp;&nbsp;");
             print("<a href='#' onclick='toggleCancel(0);'>No</a></p>");
         print("</div>");
-?>
+    ?>
     </div>
         <!-- Script to confirm order cancellation -->
         <script>
---
+++ sold_history.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+include_once('includes/connection.php');
+include_once('includes/product.php');
+include_once('includes/user.php');
+
+//instantiate Product and User class.
+$product = new Product();
+$user = new User();
+
+//Check if the user has logged in
+if(isset($_SESSION['logged_in'])) {
+    $page_title = "View Sold History";
+    include_once('includes/header.php');
+    $user_id = $_SESSION['user_id'];
+    $query = $pdo->prepare("select o.id id, o.date_time datetime, pd.name productName from campustore.orders o, campustore.product pd where o.product_id=pd.id and pd.user_id = ? and o.status = ? ORDER BY o.id DESC ");
+    $query->bindValue(1, $user_id);
+    $query->bindValue(2, 'Delivered');
+    $query->execute() or die(print_r($query->errorInfo()));
+
+    $orders = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    print("<br>&nbsp;&nbsp;<a href='account_menu.php' id='account'>Your Account</a>&nbsp;&gt;&nbsp;<span style='color: indianred'>Sold History</span>");
+    print("<div class='box' xmlns='http://www.w3.org/1999/html'>");
+        print("<h3>List of products sold</h3>");
+        print("<table><tr><th>Order Id</th><th>Product</th><th>Date</th></tr>");
+        foreach($orders as $row) {
+            print("<tr>");
+            print("<td>".$row['id']."</td><td><a href='order_detail.php?order_id=".$row['id']."'>".$row['productName']."</a></td><td>".$row['datetime']."</td>");
+            print("</tr>");
+        }
+        print("</table>");
+    print("</div>");
+    include('includes/footer.php');
+}else {
+    include('includes/signin.php');
+}
+?>
